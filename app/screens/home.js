fafc226ba08b0cbd414199455a924c57c81f593e
dperille <dannyperille@gmail.com>
2020-12-18 11:04:52 -0600

    Append element to all connected devices on button press

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 import io from 'socket.io-client';
 import { IP } from '../secrets.js';
@@ -8,20 +8,56 @@ export class HomeScreen extends React.Component {
 
     constructor(props){
         super(props);
+
+        this.state = {
+            queue: [],
+        };
+
+        this.onReceivedMessage = this.onReceivedMessage.bind(this);
+        this.onSend = this.onSend.bind(this);
+
+        this.socket = io(IP + ':3000');
+        this.socket.on('message', this.onReceivedMessage);
     }
 
-    componentDidMount() {
-        console.log("Mounted");
-        this.socket = io(IP + ':3000');
-        this.socket.emit('message', 'Hello from the app!');
+    onSend(message) {
+        this.socket.emit('message', message);
+    }
+
+    onReceivedMessage(message) {
+        this.setState({
+            queue: this.state.queue.concat([message]),
+        });
+
+        console.log("Client received: " + message);
     }
 
     render() {
+        let list_of_messages = this.state.queue.map( (data, index) => {
+            return (
+                <View key={index} style={{height: 50, width: '100%', backgroundColor: 'green'}}>
+                    <Text>{index}</Text>
+                </View>
+            )
+        })
+
         return (
-            <View>
+            <>
+            <TouchableOpacity onPress={() => this.onSend("SENDING MESSAGE!!!")} style={styles.container}>
                 <Text>Hi</Text>
-            </View>
+            </TouchableOpacity>
+            {list_of_messages}
+            </>
         )
     }
-
 }
+
+const styles = StyleSheet.create({
+    container: {
+        height: '25%',
+        width: '100%',
+        backgroundColor: 'blue',
+    },
+
+    
+});
--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
 var app = require("express")();
 var server = require("http").createServer(app);
 var io = require('socket.io')(server);
-port = process.env.PORT || 3000;
+port = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('socket id ' + socket.id + ' connected');
+io.on('connection', function(socket) {
+  console.log('socket id ' + socket.id + ' connected');
 
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
-    });
+  socket.on('message', function(msg){
+    io.emit('message', msg);
   });
+});
 
-server.listen(port, () => {
-    console.log('listening on port: ' + port);
+server.listen(port, '0.0.0.0', () => {
+  console.log('listening on port: ' + port);
 });
